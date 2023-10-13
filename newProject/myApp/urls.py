0aa40b35a0554c7a76aa0d1a35c38cd56c5871ca
Injamul Haque Navid <injamulhaque1223@gmail.com>
2023-10-14 00:23:08 +0600

    final emergency left

--- newProject/myApp/urls.py
+++ newProject/myApp/urls.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
-urlpatterns = [
+urlpatterns = [    
+    #admin and basic urls
     path('admin/', admin.site.urls),
-    path('custom_error/', views.custom_error, name='custom_error'),
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
     
+    #user acc urls
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('user_profile/', views.user_profile, name='user_profile'),
     path('logout/', views.logout, name='logout'),
     
+    #product urls
     path('products/', views.products, name='products'),
     path('product_search/', views.product_search, name='product_search'),
     path('cart/', views.cart, name='cart'), 
@@ -22,11 +26,16 @@ urlpatterns = [
     path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout'), 
     
+    #doctor appointment urls
     path('appointment/', views.appointment, name='appointment'),
     path('doctor_search/', views.doctor_search, name='doctor_search'),
     path('create_appointment/<int:doctor_id>/', views.create_appointment, name='create_appointment'),
     path('cancel_appointment/<int:appointment_id>/<int:doctor_id>/', views.cancel_appointment, name='cancel_appointment'),
     
+    #emergency urls not done
     path('emergency/', views.emergency, name='emergency'),
-    path('about/', views.about, name='about')
+    
+    #error handaling urls
+    path('custom_error/', views.custom_error, name='custom_error'),
+    path('<path:undefined_path>/', RedirectView.as_view(url='/custom_error/')),
 ]
